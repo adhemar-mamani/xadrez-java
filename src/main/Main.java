6b93469abc293dd64996a21986584b78dfce08fb
Adhemar Mamani <adhemardev@gmail.com>
2022-07-08 02:04:49 -0300

    First class: Position

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,15 @@
 package main;
 
+import boardgame.Position;
+
 public class Main {
 
 	public static void main(String[] args) {
+
+		Position pos = new Position(3, 5);
 		
-		
-		
+		System.out.println(pos);
+
 	}
-	
+
 }
