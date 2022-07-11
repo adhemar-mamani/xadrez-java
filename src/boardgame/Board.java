acbcd865610b4a22d2041d325684a0c8f70300b1
Adhemar Mamani <adhemardev@gmail.com>
2022-07-11 14:31:30 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -19,20 +19,16 @@ public class Board {
 		return rows;
 	}
 
-	
-
 	public int getColumns() {
 		return columns;
 	}
-
-	
 
 	public Piece piece(int row, int column) {
 
 		if (!positionExists(row, column)) {
 			throw new BoardException("Position not on the board");
 		}
-		
+
 		return pieces[row][column];
 	}
 
@@ -41,14 +37,14 @@ public class Board {
 		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board");
 		}
-		
+
 		return pieces[position.getRow()][position.getColumn()];
 	}
 
 	public void placepiece(Piece piece, Position position) {
-		
+
 		if (thereIsAPiece(position)) {
-			throw new BoardException("There is already a piece on position "+position);
+			throw new BoardException("There is already a piece on position " + position);
 		}
 
 		pieces[position.getRow()][position.getColumn()] = piece;
@@ -57,9 +53,23 @@ public class Board {
 
 	}
 
-	public boolean positionExists(int row, int column) {
-
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
 		
+		if (piece(position) == null) {
+			return null;
+		}
+		
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		
+		return aux;
+	}
+
+	public boolean positionExists(int row, int column) {
 
 		return (row >= 0 && row < rows && column >= 0 && column < columns);
 
@@ -76,7 +86,7 @@ public class Board {
 		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board");
 		}
-		
+
 		return piece(position) != null;
 
 	}
