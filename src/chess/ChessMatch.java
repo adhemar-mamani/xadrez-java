a6a88c27c3cc3352dd116b2f6fbd04250b71bdf8
Adhemar Mamani <adhemardev@gmail.com>
2022-07-12 02:06:36 -0300

    Switching player each turn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,12 +8,24 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
 
+	private int turn;
+	private Color currentPlayer;
 	private Board board;
 
 	public ChessMatch() {
 
 		board = new Board(8, 8);
+		turn = 1;
+		currentPlayer = Color.WHITE;
 		initialSetup();
+	}
+
+	public int getTurn() {
+		return turn;
+	}
+
+	public Color getCurrentPlayer() {
+		return currentPlayer;
 	}
 
 	public ChessPiece[][] getPieces() {
@@ -36,9 +48,10 @@ public class ChessMatch {
 		Position target = targetPosition.toPosition();
 
 		validateSourcePosition(source);
-		validateTargetPosition(source,target);
+		validateTargetPosition(source, target);
 
 		Piece capturedPiece = makeMove(source, target);
+		nextTurn();
 
 		return (ChessPiece) capturedPiece;
 
@@ -58,6 +71,12 @@ public class ChessMatch {
 			throw new ChessException("There is no piece on source position");
 
 		}
+		
+		if (currentPlayer != ((ChessPiece)board.piece(position)).getColor()) {
+			
+			throw new ChessException("the chosen piece is not yours");
+			
+		}
 		if (!board.piece(position).isThereAnyPossibleMove()) {
 
 			throw new ChessException("There is no possible moves for the chosen piece");
@@ -71,16 +90,22 @@ public class ChessMatch {
 		if (!board.piece(source).possibleMove(target)) {
 
 			throw new ChessException("The chosen piece can't move to target position");
-			
+
 		}
 
 	}
-	
-	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+
+	private void nextTurn() {
+		turn++;
 		
+		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
+	}
+
+	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+
 		Position position = sourcePosition.toPosition();
 		validateSourcePosition(position);
-		
+
 		return board.piece(position).possibleMoves();
 	}
 
--- src/main/UI.java
+++ src/main/UI.java
@@ -4,6 +4,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -94,6 +95,15 @@ public class UI {
 			}
 		}
 		System.out.print(" ");
+	}
+	
+	public static void printMatch(ChessMatch chessMatch) {
+
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.println("Turn: "+chessMatch.getTurn());
+		
+		System.out.println("Waiting player: "+ chessMatch.getCurrentPlayer());
 	}
 
 }
